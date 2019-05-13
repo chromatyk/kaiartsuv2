fa440ddd8a9ac6152ac46001817a3f740fd7f2b8
LexaeART <astheae@gmail.com>
2019-05-13 10:59:14 +0200

    Contact

---
+++ contact.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <!--        <base href="https://www.kaiartsu.fr" />-->
+  <!--        <meta name="generator" content="Jekyll v3.8.5" />-->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Kaiartsu est une agence web basée en Hauts-de-France. Spécialisée dans le développement de site web et d'identité visuelle." />
+  <meta name="publisher" content="Kaiartsu">
+  <meta name="author" content="Kaiartsu" />
+  <meta name="reply-to" content="contact@kaiartsu.fr">
+  <meta name="keywords" content="kaiartsu ; agence web hauts-de-france ; Oise ; 60 ; 02 ; 80 ; création site internet ; référencement ; webdesign ; création d'identité visuelle à Noyon ; agence digitale ; graphiste ; e-marketing ; applications mobiles" />
+  <title>Kaiartsu | L'agence web symbiotique | Création de sites web | Identité visuelle | Hauts-de-France, Noyon</title>
+  <!-- Link CSS -->
+  <link href="assets/lib/bootstrap/css/bootstrap.css" rel="stylesheet" />
+  <link href="assets/css/animate.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/contact.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/button.css" rel="stylesheet" type="text/css"/>
+  <link href="assets/css/footer.css" rel="stylesheet" type="text/css"/>
+  <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+</head>
+<body>
+  <div id="contenuLoader" class="container-fluid">
+    <div class="Loader" data-text="Loading">
+      <div class="progress-container">
+        <div class="progress">
+          <div class="progress-bar">
+            <div class="progress-shadow"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Page Home - Début -->
+  <div id="fadeSite">
+    <!-- Menu Principal - Début -->
+    <header id="header-main">
+      <a href="#" class="brand"><img src="assets/images/logo/Kaiartsuv2.png"/></a>
+      <a href="#" id="burger"> <span></span> <span></span> <span></span> </a>
+
+      <nav id="nav-main">
+        <ul>
+          <li><a href="accueil.php">Accueil</a></li>
+          <li><a href="agency.php">L'agence</a></li>
+          <li><a href="webDev.php">Développement web</a></li>
+          <li><a href="graphisme.php">Design Visuel</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <div id="nav-foot">
+
+          <div class="col"><h5>Pierre</h5>06.XX.XX.XX.XX</div>
+          <div class="col"><h5>Nicolas</h5> 07.XX.XX.XX.XX</div>
+
+        </div>
+      </nav>
+    </header>
+    <main id="contenuPage">
+      <!-- Menu Principal - Fin -->
+      <div class="container-fluid">
+        <section id="coordonnées">
+          <div class="container">
+            <div class="row">
+              <div id="adress" class="col-lg-5">
+                <h3>Noyon</h3>
+                <div id="adressAgence">
+                  <p>
+                    1435, boulevard Cambronne
+                  </p>
+                  <p>
+                    60100 NOYON
+                  </p>
+                </div>
+                <div id="devis">
+                  <p>
+                    Demande de devis en ligne
+                  </p>
+                </div>
+              </div>
+              <div id="phoneNum" class="col-lg-offset-3 col-lg-4">
+
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="contact">
+          <div class="container-fluid">
+            <div class="row">
+              <div id="sendMessage" class="col-lg-6">
+                  <form>
+                    <div class="row">
+                    <div class="form-group col-lg-5">
+                      <label for="inputName">Entrez votre nom</label>
+                      <input type="text" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Entrez votre nom">
+                    </div>
+                    <div class="form-group col-lg-5">
+                      <label for="inputMail">Entrez votre mail</label>
+                      <input type="email" class="form-control" id="inputMail" aria-describedby="mailHelp" placeholder="Entrez votre mail">
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="form-group col-lg-11">
+                      <label for="inputObject">Titre de votre message</label>
+                      <input type="text" class="form-control" id="inputObject" aria-describedby="objectHelp" placeholder="Entrez votre objet">
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="form-group col-lg-11">
+                      <label for="inputMessage">Titre de votre message</label>
+                      <textarea type="text" class="form-control" id="inputMessage" aria-describedby="messageHelp" placeholder="Entrez votre message"></textarea>
+                    </div>
+                    </div>
+                  </form>
+              </div>
+              <div id="map" class="col-lg-6">
+
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+    </main>
+  </div>
+  <script src="assets/js/design.js" type="text/javascript"></script>
+  <script src="assets/lib/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+  <script src="assets/js/onescroll.js" type="text/javascript"></script>
+  <script src="assets/js/parallax.js" type="text/javascript"></script>
+</body>
+</html>
